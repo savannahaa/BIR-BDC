4bcb720c82cf19ca35b51a8a265fcbad5ed3e16e
savannahaa <147921921+savannahaa@users.noreply.github.com>
2025-06-12 12:48:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-#include "tests/UnitTests.h"
 #include "perf.h"
-
-#include "messagePassingExample.h"
-#include "networkSocketExample.h"
-#include "volePSI/fileBased.h"
-#include "tests/Paxos_Tests.h"
-
 int main(int argc, char** argv)
 {
     perf(cmd);
